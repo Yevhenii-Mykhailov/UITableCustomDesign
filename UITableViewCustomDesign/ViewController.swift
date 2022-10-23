7f1bd8215bf4b477b19c7389ada6156565b5b2b8
Yevhenii Mykhailovskyi <mihaylovskiyeugeny@gmail.com>
2022-10-23 17:19:43 +0300

    Initial Commit

---
+++ UITableViewCustomDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewCustomDesign
+//
+//  Created by Yevhenii M on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
